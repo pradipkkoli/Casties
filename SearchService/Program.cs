550be13a3deb82bde4b1cc42181daa1e2054ae97
Pradip Koli <pradip.koli@gmail.com>
2025-08-29 18:23:01 +0530

    End of section 3

---
+++ SearchService/Program.cs
@@ -0,0 +1,50 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+
+app.MapControllers();
+
+//await DB.InitAsync("SearchDb", MongoClientSettings
+//    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+//await DB.Index<Item>()
+//    .Key(x => x.Make, KeyType.Text)
+//    .Key(x => x.Model, KeyType.Text)
+//    .Key(x => x.Color, KeyType.Text)
+//    .CreateAsync();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
